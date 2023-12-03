974eaa9ebefafc5b2b64ee1147fe61985da90ecc
Nimesh Ranasinghe <111418364+07Nimesh01Ranasinghe@users.noreply.github.com>
2023-12-03 22:55:29 +0530

    Create script.js file

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+const typed =  new Typed('.multiple-text',{
+    strings: ['Tours', 'Airport Pickups', 'Accommadation','Airport Drops'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
